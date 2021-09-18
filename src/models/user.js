b025a756ca7c0f99b931ad526e5e0fc81dac04a5
long ho <long_ho@datahouse.com>
2021-09-18 21:46:34 +0700

    Commit - 18.09.2021

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -47,6 +48,14 @@ const userSchema = new mongoose.Schema({
       required: true
     }
   }]
+}, {
+  timestamps: true
+});
+
+userSchema.virtual('tasks', {
+  ref: 'Task',
+  localField: '_id',
+  foreignField: 'owner'
 });
 
 userSchema.methods.generateAuthToken = async function () {
@@ -85,6 +94,7 @@ userSchema.statics.findByCredentials = async (email, password) => {
   return user;
 };
 
+// Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
   const user = this;
 
@@ -92,6 +102,13 @@ userSchema.pre('save', async function (next) {
     user.password = await bcrypt.hash(user.password, 8);
   }
 
+  next();
+});
+
+// Delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+  const user = this;
+  await Task.deleteMany({ owner: user._id });
   next();
 });
 
